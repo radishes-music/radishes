48156b3203b644b37c5d01f5bdc2fba92fcee04b
Link <link19970507@gmail.com>
2020-10-09 18:11:30 +0800

    ci(api): api support

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'music'
