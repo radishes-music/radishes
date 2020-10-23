8a7546bd452991fc813852387fc42627f9fc1197
little-buddy <litoubuddy@gmail.com>
2020-10-23 13:45:30 +0800

    fixed(FC): inject variable error

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,12 +1,12 @@
-import { inject, provide, unref } from 'vue'
-import { Modal, notification } from 'ant-design-vue'
-import { Message } from 'v-easy-components'
+import { inject, provide } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import { Modal, notification } from 'ant-design-vue'
+import { Notification } from 'ant-design-vue/types/notification'
+import { ModalConfirm, ModalOptions } from 'ant-design-vue/types/modal'
+import { Message } from 'v-easy-components'
+import { VeMessage } from 'v-easy-components/types/message'
 
-// const $ROUTER = Symbol()
-// const $ROUTE = Symbol()
-// const $STORE = Symbol()
 const $ERROR = Symbol()
 const $INFO = Symbol()
 const $SUCCESS = Symbol()
@@ -17,12 +17,6 @@ const $DESTROYALL = Symbol()
 const $MESSAGE = Symbol()
 
 export const allInject = () => {
-  /* vue-router */
-  // provide($ROUTER, router)
-  // provide($ROUTE, unref(router.currentRoute))
-  // /* vuex */
-  // provide($STORE, store)
-  /* ant-design-vue */
   provide($ERROR, Modal.error)
   provide($INFO, Modal.info)
   provide($SUCCESS, Modal.success)
@@ -33,12 +27,14 @@ export const allInject = () => {
   provide($MESSAGE, Message)
 }
 
+type ModalCall = (options: ModalOptions) => ModalConfirm
+
 export { useStore, useRoute, useRouter }
 export const useError = () => inject($ERROR)
-export const useInfo = () => inject($ERROR)
-export const useSuccess = () => inject($ERROR)
-export const useWarning = () => inject($ERROR)
-export const useConfirm = () => inject($ERROR)
-export const useNotification = () => inject($ERROR)
-export const useDestroyAll = () => inject($ERROR)
-export const useMessage = () => inject($ERROR)
+export const useInfo = () => inject($INFO)
+export const useSuccess = () => inject($SUCCESS) as ModalCall
+export const useWarning = () => inject($WARNING) as ModalCall
+export const useConfirm = () => inject($CONFIRM) as ModalCall
+export const useNotification = () => inject($NOTIFICATION) as Notification
+export const useDestroyAll = () => inject($DESTROYALL) as () => void
+export const useMessage = () => inject($MESSAGE) as VeMessage
