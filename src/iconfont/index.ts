6ef62c13b62d2ce8ede1dccdefc74cc22ddd8a6d
Link <linkorgs@163.com>
2020-11-28 19:04:30 +0800

    build: fix the iconfont protocol issue during the electron build process

--- src/iconfont/index.ts
+++ src/iconfont/index.ts
@@ -2,4 +2,4 @@ import $script from 'scriptjs'
 import { noop } from '@/utils/index'
 
 // repair electron packaging '//' protocol problem
-$script('//at.alicdn.com/t/font_2132275_2fbtdtwvogo.js', noop)
+$script('https://at.alicdn.com/t/font_2132275_2fbtdtwvogo.js', noop)
