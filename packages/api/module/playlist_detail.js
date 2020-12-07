d26e2ec2af9c8643ebe87091f24538daef550619
Link <link19970507@gmail.com>
2020-12-07 15:56:38 +0800

    fix: repair the playlist only supports the first 10 data

--- packages/api/module/playlist_detail.js
+++ packages/api/module/playlist_detail.js
@@ -11,5 +11,28 @@ module.exports = (query, request) => {
     cookie: query.cookie,
     proxy: query.proxy,
     realIP: query.realIP,
+  }).then(async (v) => {
+    const { body } = v
+    const { tracks, trackIds } = body.playlist
+    if (trackIds && tracks && trackIds.length > tracks.length) {
+      const ids = trackIds.slice(tracks.length).map((item) => item.id)
+      const data = {
+        c: '[' + ids.map((id) => '{"id":' + id + '}').join(',') + ']',
+        ids: '[' + ids.join(',') + ']',
+      }
+      const { body } = await request(
+        'POST',
+        `https://music.163.com/weapi/v3/song/detail`,
+        data,
+        {
+          crypto: 'weapi',
+          cookie: query.cookie,
+          proxy: query.proxy,
+          realIP: query.realIP,
+        },
+      )
+      v.body.playlist.tracks = v.body.playlist.tracks.concat(body.songs)
+    }
+    return v
   })
 }
