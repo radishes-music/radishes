d80af173e19a8c8cc79450c302c161943d952312
Link <link19970507@gmail.com>
2020-12-07 15:55:27 +0800

    style: logo design

--- src/iconfont/index.ts
+++ src/iconfont/index.ts
@@ -1,7 +1,7 @@
 import $script from 'scriptjs'
 import { noop } from '@/utils/index'
 
-const ICONFONT_URL = 'font_2132275_l43m9hqmon'
+const ICONFONT_URL = 'font_2132275_ybsarqqzpm'
 
 // repair electron packaging '//' protocol problem
 $script(`https://at.alicdn.com/t/${ICONFONT_URL}.js`, noop)
