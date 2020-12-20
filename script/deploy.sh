6eeaae26cb3b6701b77ac5f0a471a54e71131bfe
Link <linkorgs@163.com>
2020-12-20 19:33:43 +0800

    test: save historical push records for easy rollback

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,12 +7,15 @@ git init
 git config user.name "Link"
 git config user.email "link19970507@gmail.com"
 
+git remote add origin $GITHUB_REPO
+
 git add .
 
 git commit -m "Github Actions auto builder at $(date +'%Y-%m-%d %H:%M:%S')"
 git branch -M main
 
-git push --force --quiet "https://$ACCESS_TOKEN@$GITHUB_REPO" main:main
+git pull origin
+git push --quiet "https://$ACCESS_TOKEN@$GITHUB_REPO" main:main
 
 echo "Successful"
 echo "online: https://hq001.github.io/"
