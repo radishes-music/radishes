7b3b1a5127b2d4165654b061c5ffb8d180f1af1a
Link <linkorgs@163.com>
2020-12-22 13:30:46 +0800

    ci: optimize the log output of dev in the CI process

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack')
 
+const CI = process.env.CI !== 'action'
+
 module.exports = {
   pages: {
     index: {
@@ -12,6 +14,7 @@ module.exports = {
     }
   },
   devServer: {
+    progress: CI,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
