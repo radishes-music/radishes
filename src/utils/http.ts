bcc0f509a13feeeea6da36bf78e840d0d8b022f2
Link <linkorgs@163.com>
2020-12-22 16:31:26 +0800

    fix: request cookie case is wrong

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -17,7 +17,7 @@ const http: AxiosInstance = Axios.create({
 
 http.interceptors.request.use(
   config => {
-    config.headers.Cookie = document.cookie
+    config.headers.cookie = document.cookie
     return config
   },
   error => {
