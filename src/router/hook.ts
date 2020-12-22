566956218cd90a64bc74cafb2a162ae06f4d9d53
Link <link19970507@gmail.com>
2020-12-22 12:09:34 +0800

    revert: fix problems caused by git do not operate

--- src/router/hook.ts
+++ src/router/hook.ts
@@ -3,7 +3,7 @@ import { isLogin, showLogin } from '@/helpers'
 
 export function hook(router: Router) {
   router.beforeEach((to, from, next) => {
-    if (to.meta.auth && isLogin()) {
+    if (to.meta.auth && !isLogin()) {
       showLogin()
       next(false)
       return
