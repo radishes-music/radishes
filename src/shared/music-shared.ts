adf975909f0d81769dcdff99480e019a1aa739bb
Link <linkorgs@163.com>
2020-12-27 19:38:51 +0800

    fix: double-click to play to clear the playback progress

--- src/shared/music-shared.ts
+++ src/shared/music-shared.ts
@@ -4,8 +4,11 @@ import { useFooterModule } from '@/modules'
 export const playMusic = () => {
   const store = useFooterModule()
 
-  return async (id: number | unknown) => {
+  return async (id: number | unknown, isStartOver = true) => {
     store.useMutations(FooterMutations.PAUES_MUSIC)
+    if (isStartOver) {
+      store.useMutations(FooterMutations.UPDATE_CURRENT_TIME, 0)
+    }
     await store.useActions(FooterActions.SET_MUSIC, id)
     store.useMutations(FooterMutations.PLAY_MUSIC)
   }
