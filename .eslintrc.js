b67394765ff250e29ef08017de10e76f27ce906d
Link <linkorgs@163.com>
2021-03-05 15:02:11 +0800

    style: modify the weight of non-essential rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'vue/html-self-closing': [
       'error',
       {
@@ -39,6 +40,8 @@ module.exports = {
       }
     ],
     'vue/one-component-per-file': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
     'vue/custom-event-name-casing': 'off'
   },
   overrides: [
