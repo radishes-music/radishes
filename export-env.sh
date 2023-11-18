c5f2617c6ad78b43aaf8d8225655cfef4c916e55
buddy <litoubuddy@gmail.com>
2023-11-19 02:55:23 +0800

    refacotr: change env echo color

--- export-env.sh
+++ export-env.sh
@@ -1,3 +1,5 @@
 export ELECTRON_MIRROR=https://npmmirror.com/mirrors/electron/
 export ELECTRON_BUILDE_RBINARIES_MIRROR=https://npmmirror.com/mirrors/electron-builder-binaries/
-echo '==============Setup Mirror Successfully=============='
+echo -e "\033[32m ===================================================== \033[0m"
+echo -e "\033[32m ==============Setup Mirror Successfully============== \033[0m"
+echo -e "\033[32m ===================================================== \033[0m"
