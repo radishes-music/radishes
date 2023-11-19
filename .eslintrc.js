6ea4e51ae917570b13633cd7a7d323fd2806c7e8
buddy <litoubuddy@gmail.com>
2023-11-19 08:02:37 +0800

    docs: cant disabled insert-before-function-space in prettier 😂

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,25 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2021,
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     parser: {
-      '<template>': 'espree',
+      '<template>': 'espree'
     },
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json']
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
+    '@vue/eslint-config-prettier'
     // './.eslintrc-auto-import.json'
   ],
   rules: {
@@ -37,15 +37,15 @@ module.exports = {
       'error',
       {
         html: {
-          component: 'always',
-        },
-      },
+          component: 'always'
+        }
+      }
     ],
     'vue/require-explicit-emits': [
       'error',
       {
-        allowProps: true,
-      },
+        allowProps: true
+      }
     ],
     'vue/one-component-per-file': 'off',
     'vue/require-default-prop': 'off',
@@ -57,23 +57,26 @@ module.exports = {
     'vue/no-reserved-component-names': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     'no-unused-vars': 'warn',
-    'no-undef': 'warn',
+    'no-undef': 'warn'
+    // bad update...
+    // https://github.com/prettier/prettier/issues/7972#issuecomment-610255024
+    // 'space-before-function-paren': 0
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
-        mocha: true,
-      },
-    },
+        mocha: true
+      }
+    }
   ],
   globals: {
     defineProps: 'readonly',
     defineEmits: 'readonly',
     defineExpose: 'readonly',
-    withDefaults: 'readonly',
-  },
+    withDefaults: 'readonly'
+  }
 }
