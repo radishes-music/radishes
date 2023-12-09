5e38da192943c3917e65cd703c44fd86d7ed5597
buddy <litoubuddy@gmail.com>
2023-12-09 10:44:16 +0800

    refactor: new build scripts

--- src/electron/service/service.ts
+++ src/electron/service/service.ts
@@ -12,14 +12,9 @@ export const findPort = () => {
 
 export const runService = () => {
   return findPort().then(n => {
-    let filename = 'api-macos'
+    let filename =
+      process.platform === 'win32' ? 'netease-api.exe' : 'netease-api'
     const port = String(n)
-    if (process.platform === 'win32') {
-      filename = 'api-win.exe'
-    }
-    if (process.platform === 'linux') {
-      filename = 'api-linux'
-    }
     const cwd = path.resolve(__dirname, '../../public/service')
     const service = childProcess.exec(filename, {
       cwd: cwd,
